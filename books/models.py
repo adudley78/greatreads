a9990063d397c24f74042e7936e28ed0339b2c0b
Adam Dudley <dudleyadam@gmail.com>
2020-02-12 16:37:58 -0500

    Create and update models and run migrations

--- books/models.py
+++ books/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from datetime import datetime
-from readers.modles import Reader
+from readers.models import Reader
 
 class Model(models.Model):
   reader = models.ForeignKey(Reader, on_delete=models.DO_NOTHING)
   title: models.CharField(max_length=200)
   author: models.CharField(max_length=200)
-  genre: models.CharField(max_length=200)
+  genre: models.TextChoices('Genre Name', 'Mystery Thriller History Romance Nonfiction')
   notes: models.TextField(blank=True)
   has_read: models.BooleanField(default=True)
   add_date: models.DateTimeField(default=datetime.now, blank=True)
-  cover_img: models.ImageField(upload_to='covers')
+  cover_img: models.ImageField(upload_to='photos/%Y/%m/%d/')
   def __str__(self):
     return self.title
--- readers/models.py
+++ readers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Reader(models.Model):
+  name = models.CharField(max_length=200)
+  profile_img = models.ImageField(upload_to='photos/%Y/%m/%d/')
+  bio = models.TextField(blank=True)
+  is_top = models.BooleanField(default=False)
+  join_date: models.DateTimeField(default=datetime.now, blank=True)
+  def __str__(self):
+    return self.name
