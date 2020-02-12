493ba2863e94b29fa4dbdc1973ed929a1ce331c7
Adam Dudley <dudleyadam@gmail.com>
2020-02-12 18:58:28 -0500

    Re-migrate and fix typos

--- books/models.py
+++ books/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from datetime import datetime
 from readers.models import Reader
 
-class Model(models.Model):
+class Book(models.Model):
   reader = models.ForeignKey(Reader, on_delete=models.DO_NOTHING)
-  title: models.CharField(max_length=200)
-  author: models.CharField(max_length=200)
-  genre: models.TextChoices('Genre Name', 'Mystery Thriller History Romance Nonfiction')
-  notes: models.TextField(blank=True)
-  has_read: models.BooleanField(default=True)
-  add_date: models.DateTimeField(default=datetime.now, blank=True)
-  cover_img: models.ImageField(upload_to='photos/%Y/%m/%d/')
+  title = models.CharField(max_length=200)
+  author = models.CharField(max_length=200)
+  genre = models.TextChoices('Genre Name', 'Mystery Thriller History Romance Nonfiction')
+  notes = models.TextField(blank=True)
+  has_read = models.BooleanField(default=True)
+  add_date = models.DateTimeField(default=datetime.now, blank=True)
+  cover_img = models.ImageField(upload_to='photos/%Y/%m/%d/')
   def __str__(self):
     return self.title
