12922cf276c0604b8e23baec56157707bcd41954
Adam Dudley <dudleyadam@gmail.com>
2020-02-13 15:37:30 -0500

    Fix typo in model and recreate migrations

--- readers/models.py
+++ readers/models.py
@@ -6,6 +6,6 @@ class Reader(models.Model):
   profile_img = models.ImageField(upload_to='photos/%Y/%m/%d/')
   bio = models.TextField(blank=True)
   is_top = models.BooleanField(default=False)
-  join_date: models.DateTimeField(default=datetime.now, blank=True)
+  join_date = models.DateTimeField(default=datetime.now, blank=True)
   def __str__(self):
     return self.name
