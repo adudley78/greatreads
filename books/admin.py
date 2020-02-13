75c98625834996cc85aa03bff918e57fcba4bf54
Adam Dudley <dudleyadam@gmail.com>
2020-02-13 15:36:45 -0500

    Update admin settings

--- books/admin.py
+++ books/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Book
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'author', 'reader')
+  list_display_links = ('id', 'title')
+  list_filter = ('reader',)
+  search_fields = ('title', 'author', 'notes')
+  list_per_page = 25
+
+admin.site.register(Book, BookAdmin)
--- readers/admin.py
+++ readers/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Reader
 
-admin.site.register(Reader)
+class ReaderAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'join_date')
+  list_display_links = ('id', 'name')
+  search_fields = ('name',)
+  list_per_page = 25
+
+admin.site.register(Reader, ReaderAdmin)
