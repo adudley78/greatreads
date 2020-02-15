9cfa3cf02aec70b847f993fec27e5487e233a16c
Adam Dudley <dudleyadam@gmail.com>
2020-02-15 11:44:14 -0500

    Make about page content dynamic

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from books.models import Book
+from readers.models import Reader
 
 def index(request):
     books = Book.objects.order_by('-add_date')
@@ -13,7 +14,16 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    readers = Reader.objects.order_by('join_date')
+
+    top_readers = Reader.objects.all().filter(is_top=True)    
+
+    context = {
+        'readers': readers,
+        'top_readers': top_readers
+    }
+
+    return render(request, 'pages/about.html', context)
 
 def featured(request):
     return render(request, 'pages/featured.html')
