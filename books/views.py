b792eff9d05eb088e2c6fc9b828b292e9f06655d
Adam Dudley <dudleyadam@gmail.com>
2020-02-19 12:41:48 -0500

    Implement search form filtering

--- books/views.py
+++ books/views.py
@@ -28,8 +28,26 @@ def book(request, book_id):
 
 def search(request):
 
+  queryset_list = Book.objects.order_by('-add_date')
+
+  if 'keywords' in request.GET:
+    keywords = request.GET['keywords']
+    if keywords:
+      queryset_list = queryset_list.filter(notes__icontains=keywords)
+      
+  if 'author' in request.GET:
+    author = request.GET['author']
+    if author:
+      queryset_list = queryset_list.filter(author__icontains=author)
+
+  if 'title' in request.GET:
+    title = request.GET['title']
+    if title:
+      queryset_list = queryset_list.filter(title__icontains=title)
+
   context = {
     'genre_choices': genre_choices,
+    'books': queryset_list
   }
 
   return render(request, 'books/search.html', context)
